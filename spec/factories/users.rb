05915424042c5f2a7b050ef59fd77a4389b2a4f1
gogojimmy <jimmy@gogojimmy.net>
2012-04-15 11:12:53 +0800

    Users spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,48 @@
 
 FactoryGirl.define do
   factory :user do
+    is_admin false
+    name "test"
+    role "personal"
+    id_no "12345678"
+    birthday "1983-01-01".to_date
+    company "test-company"
+    capital 1000000
+    industry_type "IT"
+    website "http://example.com"
+    title "manager"
+    info "its my account"
+    email "test@example.com"
+    phone "0975519929"
+  end
+  factory :admin, class: User do
+    is_admin true
+    name "admin"
+    role "personal"
+    id_no "12345678"
+    birthday "1983-01-01".to_date
+    company "test-company"
+    capital 1000000
+    industry_type "IT"
+    website "http://example.com"
+    title "manager"
+    info "its my account"
+    email "test@example.com"
+    phone "0975519929"
+  end
+  factory :company, class: User do
+    is_admin false
+    name "A company"
+    role "company"
+    id_no "12345678"
+    birthday "1983-01-01".to_date
+    company "test-company"
+    capital 1000000
+    industry_type "IT"
+    website "http://example.com"
+    title "manager"
+    info "its my account"
+    email "test@example.com"
+    phone "0975519929"
   end
 end
