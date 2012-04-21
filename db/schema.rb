8f7980ab334237dd91536a81f999c1523b02f3e3
gogojimmy <jimmy@gogojimmy.net>
2012-04-22 01:48:17 +0800

    routes, view, I18n for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120414170207) do
+ActiveRecord::Schema.define(:version => 20120421075855) do
 
   create_table "users", :force => true do |t|
     t.boolean  "is_admin",               :default => false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20120414170207) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.string   "address"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
