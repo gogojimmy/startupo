d8a6159a0f29a9ca6b19b10edd1946c32aef202d
gogojimmy <jimmy@gogojimmy.net>
2012-04-22 01:47:10 +0800

    User attributes validation and specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_protected :is_admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :id_no, :mobile, :company, :title, :address, :name
+  validates_format_of :id_no, :with => /^\w[12]\d{8}$/
+  validates_format_of :email,
+                      :with => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  validates_presence_of :email, :password, :name
+  validates_uniqueness_of :id_no, :email
+
 end
