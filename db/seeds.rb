55e33621b3ab359acfe16f26c2ba2090c095d83e
gogojimmy <jimmy@gogojimmy.net>
2012-07-11 11:03:36 +0800

    Modify Seed data and fix admin/events bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@
 
 User.create!(:email => "admin@startupo.cc", :password => "startupoisawesome",
              :password_confirmation => "startupoisawesome",
-             :id_no => "A123456789", :name => "startupo", :is_admin => true)
+             :id_no => "A123456789", :name => "startupo", :is_admin => true,
+             :address => 'blah', :title => 'BLAH', :address => 'blah',
+             :industry => 'it', :company => 'Startupo')
 
 ResourceType.create!(:name => "資金", :description => "資金")
 ResourceType.create!(:name => "團隊", :description => "團隊")
@@ -32,12 +34,3 @@ CooperateWay.create!(:name => "融資", :description => "融資")
 CooperateWay.create!(:name => "尋找執行團隊", :description => "尋找執行團隊")
 CooperateWay.create!(:name => "學術合作", :description => "學術合作")
 CooperateWay.create!(:name => "其他", :description => "其他")
-
-AdminCategory.create!(:name => "找尋資金", :description => "找尋資金")
-AdminCategory.create!(:name => "找尋團隊", :description => "找尋團隊")
-AdminCategory.create!(:name => "找尋製造技術", :description => "找尋製造技術")
-AdminCategory.create!(:name => "找尋相關通路", :description => "找尋相關通路")
-AdminCategory.create!(:name => "找尋營銷策略", :description => "找尋營銷策略")
-AdminCategory.create!(:name => "找尋人脈資源", :description => "找尋人脈資源")
-AdminCategory.create!(:name => "找尋活動配合", :description => "找尋活動配合")
-AdminCategory.create!(:name => "找尋策略聯盟", :description => "找尋策略聯盟")
